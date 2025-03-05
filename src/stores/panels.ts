90201b0e7e318ff104a8957b4349072d38a404ee
david.delrio <david.delrio@congreso.es>
2025-03-05 17:12:01 +0100

    Add solar panels store that communicates with the backend.
    - Only fetching for now.

---
+++ src/stores/panels.ts
@@ -0,0 +1,43 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import axios, { AxiosError } from 'axios'
+
+interface Panel {
+  id?: string;
+  model: string;
+  nominal_power: number;
+  vmpp: number;
+  impp: number;
+  voc: number;
+  isc: number;
+  length: number;
+  width: number;
+  reference?: string;
+  description?: string;
+}
+
+export const usePanelsStore = defineStore('panels', () => {
+  const panels = ref<Panel[]>([])
+  const fetching = ref(false);
+  const error = ref(false);
+  const errorDetails = ref<AxiosError>();
+
+  const fetchPanels = ()=> {
+    fetching.value = true;
+    axios
+      .get('/panels/')
+      .then(response => {
+        panels.value = response.data;
+        error.value = false;
+      })
+      .catch((errorResponse) => {
+        error.value = true;
+        errorDetails.value = errorResponse;
+      })
+      .finally(() => {
+        fetching.value = false;
+      })
+  }
+
+  return { panels, fetching, error, errorDetails, fetchPanels }
+})
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>List of panels on the database</h2>
+    <h3 v-if="panelsStore.error">😿 There was an error fetching the data from the backend:
+      {{ panelsStore.errorDetails?.message }}</h3>
+    <p v-else>
+      {{ panelsStore.panels }}
+    </p>
   </div>
 </template>
 
@@ -8,8 +14,17 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
     align-items: center;
   }
 }
 </style>
+
+<script setup lang="ts">
+  import { usePanelsStore } from '@/stores/panels.ts'
+  import { onMounted } from 'vue'
+  const panelsStore = usePanelsStore()
+
+  onMounted(() => {
+    panelsStore.fetchPanels()
+  });
+</script>
