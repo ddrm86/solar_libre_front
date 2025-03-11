2d2740e3732e3b942e972b327acc67951b1fe50c
david.delrio <david.delrio@congreso.es>
2025-03-11 10:38:49 +0100

    Add primevue Toast Service

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ axios.defaults.timeout = 2000;
 </script>
 
 <template>
+  <Toast />
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import App from './App.vue'
 import router from './router'
 
 import PrimeVue from 'primevue/config';
+import DialogService from 'primevue/dialogservice'
+import ToastService from 'primevue/toastservice';
 import Aura from '@primevue/themes/aura';
 import { createI18n } from 'vue-i18n'
 
@@ -21,5 +23,7 @@ app.use(PrimeVue, {
     preset: Aura
   }
 });
+app.use(DialogService);
+app.use(ToastService);
 
 app.mount('#app')
