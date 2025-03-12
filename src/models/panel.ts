bb23c36ea4e56cbe1f799bf8bd518e13564ed701
david.delrio <david.delrio@congreso.es>
2025-03-12 17:04:07 +0100

    Improve add/edit dialog style.

---
+++ src/models/panel.ts
@@ -0,0 +1,42 @@
+export interface Panel {
+  id?: string;
+  model: string;
+  nominal_power: number;
+  vmpp: number;
+  impp: number;
+  voc: number;
+  isc: number;
+  length: number;
+  width: number;
+  reference?: string;
+  description?: string;
+  deleted: boolean;
+}
+
+export const validatePanel = (panel: Panel): boolean => {
+  if (!panel.model?.trim()) {
+    return false;
+  }
+  if (!panel.nominal_power) {
+    return false;
+  }
+  if (!panel.vmpp) {
+    return false;
+  }
+  if (!panel.impp) {
+    return false;
+  }
+  if (!panel.voc) {
+    return false;
+  }
+  if (!panel.isc) {
+    return false;
+  }
+  if (!panel.length) {
+    return false;
+  }
+  if (!panel.width) {
+    return false;
+  }
+  return true;
+};
