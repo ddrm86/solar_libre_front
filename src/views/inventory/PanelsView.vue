698ecbcf8b83bc4575fb0053eacf00f4f8275813
david.delrio <david.delrio@congreso.es>
2025-03-25 10:01:12 +0100

    Move PanelsTable component to its own view.

---
+++ src/views/inventory/PanelsView.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <PanelsTable />
+</div>
+</template>
+
+<script setup lang="ts">
+import PanelsTable from '@/components/inventory/PanelsTable.vue'
+</script>
+
+<style scoped>
+
+</style>
