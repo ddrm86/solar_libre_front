6d897ef0e9efc5f66a90cbfefbd6e79a658109e7
David del Río Medina <ddrm86@gmail.com>
2025-04-09 18:49:01 +0200

    Add complete PVGIS response data model.

--- src/models/pvgis.ts
+++ src/models/pvgis.ts
@@ -18,18 +18,53 @@ export interface PvgisRequest {
   azimuth: number;
 }
 
+/**
+ * Data model for PVGIS response parameters.
+ *
+ * Attributes:
+ *   database (string): The database used for the calculations.
+ *   monthly (Array): Array of monthly data containing energy production and irradiation details.
+ *     - month (number): The month of the year (1-12).
+ *     - E_d (number): Average daily energy production from the system (kWh/d).
+ *     - E_m (number): Average monthly energy production from the system (kWh/mo).
+ *     - H(i)_d (number): Average daily sum of global irradiation per square meter received by the modules (kWh/m²/d).
+ *     - H(i)_m (number): Average monthly sum of global irradiation per square meter received by the modules (kWh/m²/mo).
+ *     - SD_m (number): Standard deviation of the monthly energy production due to year-to-year variation (kWh).
+ *   totals (Object): Total energy production and irradiation details.
+ *     - E_d (number): Average daily energy production from the system (kWh/d).
+ *     - E_m (number): Average monthly energy production from the system (kWh/mo).
+ *     - E_y (number): Average annual energy production from the system (kWh/y).
+ *     - H(i)_d (number): Average daily sum of global irradiation per square meter received by the modules (kWh/m²/d).
+ *     - H(i)_m (number): Average monthly sum of global irradiation per square meter received by the modules (kWh/m²/mo).
+ *     - H(i)_y (number): Average annual sum of global irradiation per square meter received by the modules (kWh/m²/y).
+ *     - SD_m (number): Standard deviation of the monthly energy production due to year-to-year variation (kWh).
+ *     - SD_y (number): Standard deviation of the annual energy production due to year-to-year variation (kWh).
+ *     - l_aoi (number): Angle of incidence loss (%).
+ *     - l_spec (string): Spectral loss (%).
+ *     - l_tg (number): Temperature and irradiance loss (%).
+ *     - l_total (number): Total loss (%).
+ */
 export interface PvgisResponse {
   database: string;
 
   monthly: [{
     month: number;
-    Ed: number;
-    Em: number;
+    E_d: number;
+    E_m: number;
   }];
 
-  yearly: {
-    Ed: number;
-    Em: number;
-    Ey: number;
+  totals: {
+    E_d: number;
+    E_m: number;
+    E_y: number;
+    Hi_d: number;
+    Hi_m: number;
+    Hi_y: number;
+    SD_m: number;
+    SD_y: number;
+    l_aoi: number;
+    l_spec: number;
+    l_tg: number;
+    l_total: number;
   };
 }
