df5bec78fcf71defaec9c2bb5823e700f613f373
david.delrio <david.delrio@congreso.es>
2025-04-14 13:35:35 +0200

    Add PVGIS data fetching functionality and response handling

--- src/models/pvgis.ts
+++ src/models/pvgis.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 /**
  * Data model for PVGIS request parameters.
  *
@@ -10,12 +12,12 @@
  *   azimuth (number): Azimuth angle of the solar panels (0 = S, 90 = W, -90 = E).
  */
 export interface PvgisRequest {
-  latitude: number;
-  longitude: number;
-  peakPower: number;
-  loss: number;
-  angle: number;
-  azimuth: number;
+  latitude: number
+  longitude: number
+  peakPower: number
+  loss: number
+  angle: number
+  azimuth: number
 }
 
 /**
@@ -45,26 +47,90 @@ export interface PvgisRequest {
  *     - l_total (number): Total loss (%).
  */
 export interface PvgisResponse {
-  database: string;
+  database: string
 
-  monthly: [{
-    month: number;
-    E_d: number;
-    E_m: number;
-  }];
+  monthly: [
+    {
+      month: number
+      E_d: number
+      E_m: number
+    },
+  ]
 
   totals: {
-    E_d: number;
-    E_m: number;
-    E_y: number;
-    Hi_d: number;
-    Hi_m: number;
-    Hi_y: number;
-    SD_m: number;
-    SD_y: number;
-    l_aoi: number;
-    l_spec: number;
-    l_tg: number;
-    l_total: number;
-  };
+    E_d: number
+    E_m: number
+    E_y: number
+    Hi_d: number
+    Hi_m: number
+    Hi_y: number
+    SD_m: number
+    SD_y: number
+    l_aoi: number
+    l_spec: number
+    l_tg: number
+    l_total: number
+  }
+}
+
+export interface PvgisInterface {
+  request: PvgisRequest
+  response?: PvgisResponse
+  fetch: () => void
+}
+
+export class Pvgis implements PvgisInterface {
+  request: PvgisRequest
+  response?: PvgisResponse
+
+  constructor(request: PvgisRequest) {
+    this.request = request
+  }
+
+  fetch() {
+    axios
+      .get('/pvgis', {
+        params: {
+          latitude: this.request.latitude,
+          longitude: this.request.longitude,
+          peak_power: this.request.peakPower,
+          loss: this.request.loss,
+          angle: this.request.angle,
+          azimuth: this.request.azimuth,
+        },
+      })
+      .then((apiResponse) => {
+        this.response = Pvgis.readPvgisResponse(apiResponse.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  private static readPvgisResponse(apiResponse: any) : PvgisResponse {
+    return {
+      database: apiResponse.inputs.meteo_data.radiation_db,
+      // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+      monthly: apiResponse.outputs.monthly.fixed.map((monthData: any) => ({
+        month: monthData.month,
+        E_d: monthData.E_d,
+        E_m: monthData.E_m,
+      })),
+      totals: {
+        E_d: apiResponse.outputs.totals.fixed.E_d,
+        E_m: apiResponse.outputs.totals.fixed.E_m,
+        E_y: apiResponse.outputs.totals.fixed.E_y,
+        Hi_d: apiResponse.outputs.totals.fixed['H(i)_d'],
+        Hi_m: apiResponse.outputs.totals.fixed['H(i)_m'],
+        Hi_y: apiResponse.outputs.totals.fixed['H(i)_y'],
+        SD_m: apiResponse.outputs.totals.fixed.SD_m,
+        SD_y: apiResponse.outputs.totals.fixed.SD_y,
+        l_aoi: apiResponse.outputs.totals.fixed.l_aoi,
+        l_spec: parseFloat(apiResponse.outputs.totals.fixed.l_spec),
+        l_tg: apiResponse.outputs.totals.fixed.l_tg,
+        l_total: apiResponse.outputs.totals.fixed.l_total,
+      },
+    }
+  }
 }
