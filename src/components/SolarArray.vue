e97d0684db0fd14e12f3ec9d3d4aaa9c4a008382
David del Río Medina <ddrm86@gmail.com>
2025-04-15 19:48:57 +0200

    Add default values and validation for PVGIS data fetching button

--- src/components/SolarArray.vue
+++ src/components/SolarArray.vue
@@ -29,6 +29,7 @@
           <InputNumber
             id="loss"
             v-model="solarArrayStore.solarArray.loss"
+            :defaultValue="14"
             :min="0"
             :max="100"
             :suffix="'%'"
@@ -41,6 +42,7 @@
           <InputNumber
             id="angle"
             v-model="solarArrayStore.solarArray.angle"
+            :defaultValue="35"
             :min="0"
             :max="90"
             :suffix="'°'"
@@ -63,6 +65,10 @@
     </Fluid>
     <Button
       :label="t('solar_array.consult_pvgis')"
+      :loading="solarArrayStore.pvgisData?.fetching"
+      :disabled="!isValidRequest"
+      :badge="badgeText"
+      :badgeSeverity="badgeSeverity"
       icon="pi pi-search"
       @click="solarArrayStore.fetchPvgisData"
     />
@@ -73,10 +79,12 @@
 import { useI18n } from 'vue-i18n'
 import { useSolarArrayStore } from '@/stores/solarArray.ts'
 import { usePanelsStore } from '@/stores/panels.ts'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import type { Panel } from '@/models/panel.ts'
+import { useProjectInfoStore } from '@/stores/projectInfo.ts'
 
 const { t } = useI18n()
+const projectInfoStore = useProjectInfoStore()
 const solarArrayStore = useSolarArrayStore()
 const panelsStore = usePanelsStore()
 
@@ -89,11 +97,57 @@ onMounted(() => {
     panelFetchingError.value = panelsStore.error
   })
 })
+
+const isValidRequest = computed(() => {
+  return (
+    projectInfoStore.projectInfo.location.latitude !== 0.0 &&
+    projectInfoStore.projectInfo.location.longitude !== 0.0 &&
+    Object.keys(solarArrayStore.solarArray.panel).length !== 0 &&
+    solarArrayStore.solarArray.panelNumber > 0
+  )
+})
+
+const badgeText = computed(() => {
+  if (!isValidRequest.value) {
+    return t('pvgis_button.missing_data')
+  } else if (solarArrayStore.pvgisData?.error) {
+    return t('pvgis_button.error')
+  } else if (solarArrayStore.pvgisData?.fetching) {
+    return t('pvgis_button.fetching')
+  } else if (solarArrayStore.isDirty) {
+    return t('pvgis_button.needs_update')
+  } else if (!solarArrayStore.isDirty && !solarArrayStore.pvgisData?.error) {
+    return t('pvgis_button.updated')
+  } else {
+    return ''
+  }
+})
+
+const badgeSeverity = computed(() => {
+  if (solarArrayStore.pvgisData?.error) {
+    return 'danger'
+  } else if (solarArrayStore.pvgisData?.fetching) {
+    return 'secondary'
+  } else if (solarArrayStore.isDirty) {
+    return 'contrast'
+  } else if (!solarArrayStore.isDirty && !solarArrayStore.pvgisData?.error) {
+    return 'success'
+  } else {
+    return 'secondary'
+  }
+})
 </script>
 
 <i18n>
 {
   "en": {
+    "pvgis_button": {
+      "missing_data": "Missing data",
+      "needs_update": "Needs update",
+      "updated": "Updated",
+      "error": "Error performing query",
+      "fetching": "Querying"
+    },
     "solar_array": {
       "title": "Solar array configuration",
       "panel": "Panel",
@@ -106,6 +160,13 @@ onMounted(() => {
     }
   },
   "es": {
+    "pvgis_button": {
+      "missing_data": "Faltan datos",
+      "needs_update": "Necesita actualización",
+      "updated": "Actualizado",
+      "error": "Error al consultar",
+      "fetching": "Consultando"
+    },
     "solar_array": {
       "title": "Configuración de la matriz solar",
       "panel": "Panel",
