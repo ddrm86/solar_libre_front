bb642b885d6654118441029a21be7fe785042d29
david.delrio <david.delrio@congreso.es>
2025-04-22 18:28:16 +0200

    Mark all solar arrays as dirty if the project location changes.

--- src/stores/solarArrays.ts
+++ src/stores/solarArrays.ts
@@ -1,13 +1,27 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { SolarArray } from '@/models/solarArray.ts'
+import { useProjectInfoStore } from '@/stores/projectInfo.ts'
+
 
 export const useSolarArraysStore = defineStore('solar_arrays', () => {
+  const projectInfoStore = useProjectInfoStore()
+
   const arrays = ref<SolarArray[]>([])
 
   const addSolarArray = () => {
     arrays.value.push(new SolarArray())
   }
+
+  watch(
+    () => projectInfoStore.projectInfo.location,
+    () => {
+      arrays.value.forEach((array) => {
+        array.isDirty = true
+      })
+    },
+    { deep: true }
+  )
 
   return { arrays, addSolarArray }
 })
