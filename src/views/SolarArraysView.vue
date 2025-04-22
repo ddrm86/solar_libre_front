d02fe0fa8da2cc2289a7c263b4feaf41705400e7
david.delrio <david.delrio@congreso.es>
2025-04-22 17:04:13 +0200

    Add SolarArray class that combines solar array data and its pvgis information.

--- src/views/SolarArraysView.vue
+++ src/views/SolarArraysView.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup lang="ts">
-import SolarArrayData from '@/components/solar_arrays/SolarArray.vue'
+import SolarArray from '@/components/solar_arrays/SolarArray.vue'
 </script>
