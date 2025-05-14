caa4335161b2fe4dca6369630e117de3869fe529
David del Río Medina <ddrm86@gmail.com>
2025-05-14 19:06:56 +0200

    Refactor yearly consumption model to use an array for monthly data and add method to set consumption values by month and time band

--- src/models/consumption/inputConsumption.ts
+++ src/models/consumption/inputConsumption.ts
@@ -1,49 +1,51 @@
 export interface IConsumptionByTimeBand {
-  peak: number,
-  flat: number,
-  valley: number,
+  peak: number
+  flat: number
+  valley: number
 }
 
 export interface IYearlyConsumption {
-  january: IConsumptionByTimeBand,
-  february: IConsumptionByTimeBand,
-  march: IConsumptionByTimeBand,
-  april: IConsumptionByTimeBand,
-  may: IConsumptionByTimeBand,
-  june: IConsumptionByTimeBand,
-  july: IConsumptionByTimeBand,
-  august: IConsumptionByTimeBand,
-  september: IConsumptionByTimeBand,
-  october: IConsumptionByTimeBand,
-  november: IConsumptionByTimeBand,
-  december: IConsumptionByTimeBand,
+  consumptionsPerMonth: [IConsumptionByTimeBand]
 
-  calcTotalConsumptionByTimeBand: () => IConsumptionByTimeBand,
-  calcTotalConsumption: () => number,
+  calcTotalConsumptionByTimeBand: () => IConsumptionByTimeBand
+  calcTotalConsumption: () => number
+  setConsumptionByMonthAndTimeBand: (
+    monthIndex: number,
+    timeBand: keyof IConsumptionByTimeBand,
+    value: number,
+  ) => void
 }
 
 export class CYearlyConsumption implements IYearlyConsumption {
-  january = { peak: 0, flat: 0, valley: 0 };
-  february = { peak: 0, flat: 0, valley: 0 };
-  march = { peak: 0, flat: 0, valley: 0 };
-  april = { peak: 0, flat: 0, valley: 0 };
-  may = { peak: 0, flat: 0, valley: 0 };
-  june = { peak: 0, flat: 0, valley: 0 };
-  july = { peak: 0, flat: 0, valley: 0 };
-  august = { peak: 0, flat: 0, valley: 0 };
-  september = { peak: 0, flat: 0, valley: 0 };
-  october = { peak: 0, flat: 0, valley: 0 };
-  november = { peak: 0, flat: 0, valley: 0 };
-  december = { peak: 0, flat: 0, valley: 0 };
+  consumptionsPerMonth: [IConsumptionByTimeBand]
+
+  constructor() {
+    this.consumptionsPerMonth = Array.from({ length: 12 }, () => ({
+      peak: 0,
+      flat: 0,
+      valley: 0,
+    })) as [IConsumptionByTimeBand]
+  }
+
+  setConsumptionByMonthAndTimeBand(
+    monthIndex: number,
+    timeBand: keyof IConsumptionByTimeBand,
+    value: number,
+  ): void {
+    if (monthIndex >= 0 && monthIndex < 12 && value >= 0) {
+      this.consumptionsPerMonth[monthIndex][timeBand] = value
+    } else {
+      throw new Error('Índice de mes o valor inválido')
+    }
+  }
 
   calcTotalConsumptionByTimeBand(): IConsumptionByTimeBand {
     const results = { peak: 0, flat: 0, valley: 0 }
-    Object.keys(this).forEach((month) => {
-      const monthConsumpPerBand = this[month as keyof CYearlyConsumption] as IConsumptionByTimeBand;
+    this.consumptionsPerMonth.forEach((monthConsumpPerBand) => {
       results.peak += monthConsumpPerBand.peak
       results.flat += monthConsumpPerBand.flat
       results.valley += monthConsumpPerBand.valley
-    });
+    })
     return results
   }
 
