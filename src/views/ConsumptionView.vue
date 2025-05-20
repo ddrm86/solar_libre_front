883a0cddf07b9760e2c726143d6c200bc4eba395
david.delrio <david.delrio@congreso.es>
2025-05-20 16:26:21 +0200

    Add PV consumption table to display estimated monthly energy consumption during photovoltaic production hours

--- src/views/ConsumptionView.vue
+++ src/views/ConsumptionView.vue
@@ -1,7 +1,9 @@
 <template>
   <InputConsumption />
+  <PVConsumptionTable />
 </template>
 
 <script setup lang="ts">
 import InputConsumption from '@/components/consumption/InputConsumption.vue'
+import PVConsumptionTable from '@/components/consumption/PvConsumptionTable.vue'
 </script>
