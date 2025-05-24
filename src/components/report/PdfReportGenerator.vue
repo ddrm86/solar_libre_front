1d6fc85f7da9a43508a734f37942848193d63138
David del Río Medina <ddrm86@gmail.com>
2025-05-24 18:11:17 +0200

    Generate sample report with i18n translations.

--- src/components/report/PdfReportGenerator.vue
+++ src/components/report/PdfReportGenerator.vue
@@ -9,7 +9,48 @@
 </template>
 
 <script setup lang="ts">
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+import type { TDocumentDefinitions } from "pdfmake/interfaces";
+import { useI18n } from "vue-i18n";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(<any>pdfMake).addVirtualFileSystem(pdfFonts);
+
+const { t } = useI18n();
+
+const pdfDefinition: TDocumentDefinitions = {
+  content: [
+    {
+      text: t("report.title"),
+      style: "header",
+    },
+  ],
+  styles: {
+    header: {
+      fontSize: 18,
+      bold: true,
+      alignment: "center",
+    },
+  },
+};
+
 const generatePdfReport = () => {
-  alert('PDF report generation is not implemented yet.')
-}
+  pdfMake.createPdf(pdfDefinition).open();
+};
 </script>
+
+<i18n>
+{
+  "en": {
+    "report": {
+      "title": "SolarLibre Report"
+    }
+  },
+  "es": {
+    "report": {
+      "title": "Informe de SolarLibre"
+    }
+  }
+}
+</i18n>
