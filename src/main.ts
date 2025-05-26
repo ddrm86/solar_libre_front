4aea569464f3bb24fa23e7bd9dfb5dd3b8fb9854
david.delrio <david.delrio@congreso.es>
2025-05-26 12:40:57 +0200

    Add language selection switch in the menu bar.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css';
+import './style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,23 +6,29 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import PrimeVue from 'primevue/config';
+import PrimeVue from 'primevue/config'
 import DialogService from 'primevue/dialogservice'
-import ToastService from 'primevue/toastservice';
-import Aura from '@primevue/themes/aura';
+import ToastService from 'primevue/toastservice'
+import Aura from '@primevue/themes/aura'
 import { createI18n } from 'vue-i18n'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(createI18n({  locale: navigator.language, fallbackLocale: 'en' }))
+app.use(
+  createI18n({
+    locale: navigator.language.split('-')[0],
+    fallbackLocale: 'en',
+    messages: { es: {}, en: {} }, // availableLocales shows just the navigation language if it is not initialized
+  }),
+)
 app.use(router)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura
-  }
-});
-app.use(DialogService);
-app.use(ToastService);
+    preset: Aura,
+  },
+})
+app.use(DialogService)
+app.use(ToastService)
 
 app.mount('#app')
