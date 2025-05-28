2ff695b2668ea3847037850a63735acdeb8b713e
david.delrio <david.delrio@congreso.es>
2025-05-28 14:48:27 +0200

    Add incomplete project selection component.

---
+++ src/components/project_persistence/ProjectSelection.vue
@@ -0,0 +1,58 @@
+<template>
+  <IftaLabel>
+    <Select
+      id="project"
+      filter
+      v-model="selectedProject"
+      :options="projectOptions"
+      :optionLabel="'label'"
+      :placeholder="t('project_selection.select_project')"
+      @change="onProjectChange"
+    />
+    <label for="project">{{ t('project_selection.project') }}</label>
+  </IftaLabel>
+</template>
+
+<script setup lang="ts">
+import { useProjectListStore } from '@/stores/project_persistence/projectList.ts'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const projectListStore = useProjectListStore()
+const selectedProject = ref(null)
+
+const projectOptions = projectListStore.availableProjects.map((project) => {
+  let address = '';
+  if (project.location.address) {
+    const truncatedAddress = project.location.address.substring(0, 40);
+    address = ` (${truncatedAddress}${project.location.address.length > 40 ? '...' : ''})`;
+  }
+  return {
+    label: project.name + address,
+    value: project.id,
+  };
+});
+
+const onProjectChange = () => {
+  console.log('Selected project:', selectedProject.value)
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "project_selection": {
+      "select_project": "Select a project",
+      "project": "Project"
+    }
+  },
+  "es": {
+    "project_selection": {
+      "select_project": "Selecciona un proyecto",
+      "project": "Proyecto"
+    }
+  }
+}
+</i18n>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
-<div>
-  <img alt="SolarLibre logo" class="logo" src="@/assets/solar_libre_logo.jpeg" width="800" height="800" />
+<div class="flex items-center">
+  <div>
+    <img alt="SolarLibre logo" class="logo" src="@/assets/solar_libre_logo.jpeg" width="800" height="800" />
+  </div>
+  <div>
+    <ProjectSelection />
+  </div>
 </div>
 </template>
 
 <script setup lang="ts">
 import { useProjectListStore } from '@/stores/project_persistence/projectList.ts'
+import ProjectSelection from '@/components/project_persistence/ProjectSelection.vue'
 
 const projectListStore = useProjectListStore()
 projectListStore.fetchProjects().then(() => console.log(projectListStore.availableProjects))
