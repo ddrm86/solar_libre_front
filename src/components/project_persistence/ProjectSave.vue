7e9e78c9b7eae822517d9f61399ee4611a120787
David del Río Medina <ddrm86@gmail.com>
2025-05-31 13:30:09 +0200

    Add toast message when project saving was successful.

--- src/components/project_persistence/ProjectSave.vue
+++ src/components/project_persistence/ProjectSave.vue
@@ -30,8 +30,13 @@ const saveProject = () => {
       })
     } else {
       saved.value = true
-    }
-  })
+      toast.add({
+        severity: 'success',
+        summary: t('messages.saving_project_success'),
+        detail: t('messages.saving_project_success_detail'),
+        life: 3000,
+      })
+    }  })
 }
 </script>
 
@@ -40,13 +45,17 @@ const saveProject = () => {
   "en": {
     "messages": {
       "saving_project_error": "Error saving the project",
-      "saving_project_error_detail": "Could not save the project: "
+      "saving_project_error_detail": "Could not save the project: ",
+      "saving_project_success": "Project saved successfully",
+      "saving_project_success_detail": "The project has been saved successfully."
     }
   },
   "es": {
     "messages": {
       "saving_project_error": "Error salvando el proyecto",
-      "saving_project_error_detail": "No se pudo guardar el proyecto: "
+      "saving_project_error_detail": "No se pudo guardar el proyecto: ",
+      "saving_project_success": "Proyecto guardado exitosamente",
+      "saving_project_success_detail": "El proyecto se ha guardado exitosamente."
     }
   }
 }
