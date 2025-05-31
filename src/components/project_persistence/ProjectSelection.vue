7a493e7c0c335dc2c1b1fbdb780b4af01ce55fcd
David del Río Medina <ddrm86@gmail.com>
2025-05-31 13:25:18 +0200

    Select on mounted the currently loaded project.

--- src/components/project_persistence/ProjectSelection.vue
+++ src/components/project_persistence/ProjectSelection.vue
@@ -26,12 +26,16 @@ import { useProjectLoadingStore } from '@/stores/project_persistence/projectLoad
 import { computed, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useToast } from 'primevue/usetoast'
+import { useProjectSavingStore } from '@/stores/project_persistence/projectSaving.ts'
+import { useProjectInfoStore } from '@/stores/project_info/projectInfo.ts'
 
 const toast = useToast()
 const { t } = useI18n()
 
 const projectListStore = useProjectListStore()
 const projectLoadingStore = useProjectLoadingStore()
+const projectSavingStore = useProjectSavingStore()
+const projecInfoStore = useProjectInfoStore()
 
 onMounted(() => {
   projectListStore.fetchProjects().then(() => {
@@ -42,6 +46,17 @@ onMounted(() => {
         detail: `${t('messages.loading_projects_error_detail')} ${projectListStore.errorDetails ?? ''}`,
         life: 5000,
       })
+    } else if (!projectSavingStore.isNewProject && projecInfoStore.projectInfo.id) {
+      const current = projecInfoStore.projectInfo
+      let addressLabel = ''
+      if (current.location.address) {
+        const truncatedAddress = current.location.address.substring(0, 40)
+        addressLabel = ` (${truncatedAddress}${current.location.address.length > 40 ? '...' : ''})`
+      }
+      selectedProject.value = {
+        label: current.name + addressLabel,
+        value: current.id ?? '',
+      }
     }
   })
 })
