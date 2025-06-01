8cbf6cc06ef8ba7c1aabc20e6d4642024b02fb5b
David del Río Medina <ddrm86@gmail.com>
2025-06-01 19:57:14 +0200

    Add quick start guide.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <ProjectSelection />
+  <Divider />
+  <article class="xl:w-1/2">
+    <QuickStartGuide />
+  </article>
 </template>
 
 <script setup lang="ts">
 import ProjectSelection from '@/components/project_persistence/ProjectSelection.vue'
+import QuickStartGuide from '@/components/QuickStartGuide.vue'
 </script>
