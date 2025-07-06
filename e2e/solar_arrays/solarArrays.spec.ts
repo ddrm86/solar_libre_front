ab8b2fcd89f9fc9212c4aae06fede04ce7f938f8
David del Río Medina <ddrm86@gmail.com>
2025-07-06 12:34:35 +0200

    Add tests to verify solar arrays page.

---
+++ e2e/solar_arrays/solarArrays.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('calculates total power production', async ({ page }) => {
+  await page.goto('/solar_arrays');
+  await page.getByRole('button', { name: 'Add solar array' }).click();
+  await page.getByRole('combobox', { name: 'Select a panel model' }).click();
+  await page.getByText('Winaico 360MGL PERC FULL').click();
+  await page.locator('#panelNumber button').first().click();
+  await page.locator('#panelNumber button').first().click();
+  await expect(page.getByLabel('', { exact: true })).toContainText('(0.72 kW)');
+})
+
+test('queries PVGIS data', async ({ page }) => {
+  await page.goto('/solar_arrays');
+  await page.getByRole('button', { name: 'Add solar array' }).click();
+  await page.getByRole('combobox', { name: 'Select a panel model' }).click();
+  await page.getByText('Winaico 360MGL PERC FULL').click();
+  await page.locator('#panelNumber button').first().click();
+  await page.locator('#panelNumber button').first().click();
+  await expect(page.getByLabel('Consult PVGIS Data')).toContainText('Needs update');
+  await page.getByRole('button', { name: 'Consult PVGIS Data' }).click();
+  await expect(page.locator('.p-card-body').first()).toBeVisible();
+  await expect(page.locator('div:nth-child(2) > .p-card-body')).toBeVisible();
+  await expect(page.getByLabel('', { exact: true })).toContainText('E_m');
+  await expect(page.getByLabel('', { exact: true })).toContainText('PROVIDED DATA');
+  await expect(page.getByLabel('Consult PVGIS Data')).toContainText('Updated');
+})
